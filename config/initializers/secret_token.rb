f004a1942f1d77f2286b2797c53dfb35022741fe
unknown <en27553@WPWS00191736.eni.pri>
2013-07-13 21:48:47 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5bc089625b2fcdd59d46e907286afc57c0f5a7275803c5f861f5da138d9debee77318bd2d4e83098f6e48b2dc461ee8b7e1fdd2f04a4f858ddb1422fe801e749'
